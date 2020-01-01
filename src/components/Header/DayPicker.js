68a70f8a50e0aa2406272391425b73d951215745
Lee Mulvey <lmulvey@me.com>
2019-12-31 22:35:59 -0700

    fix: codemods

--- src/components/Header/DayPicker.js
+++ src/components/Header/DayPicker.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import moment from 'moment';
 import ReactDayPicker from 'react-day-picker';
@@ -19,66 +19,64 @@ export const timeOptions = [
   { key: 'one-pm', text: '1:00pm', value: '1' },
 ];
 
-class DayPicker extends Component {
-  render() {
-    const {
-      selectedDay,
-      selectedTime,
-      position,
-      message,
-      onClickNextSteps,
-      handleDayClick,
-      onChangeSelectedTime,
-    } = this.props;
-    const prettyDay = selectedDay
-      ? moment(selectedDay).format('MMM DD, YYYY')
-      : '';
-    const prettyTime = selectedTime
-      ? timeOptions.find(time => time.value === selectedTime)
-      : '';
+const DayPicker = props => {
+  const {
+    selectedDay,
+    selectedTime,
+    position,
+    message,
+    onClickNextSteps,
+    handleDayClick,
+    onChangeSelectedTime,
+  } = props;
+  const prettyDay = selectedDay
+    ? moment(selectedDay).format('MMM DD, YYYY')
+    : '';
+  const prettyTime = selectedTime
+    ? timeOptions.find(time => time.value === selectedTime)
+    : '';
 
-    return (
-      <StageContainer position={position}>
-        <ReactDayPicker
-          onDayClick={handleDayClick}
-          selectedDays={selectedDay}
-          modifiers={{ isHoliday, hasPassed }}
-          disabledDays={{
-            daysOfWeek: [0, 6],
-          }}
-        />
-        {selectedDay ? (
-          <BottomContainer>
-            {prettyDay}
-            <Form size="mini">
-              <Form.Group>
-                <Form.Field
-                  control={Select}
-                  options={timeOptions}
-                  placeholder="What time?"
-                  value={selectedTime}
-                  onChange={onChangeSelectedTime}
-                />
-              </Form.Group>
-            </Form>
-            {selectedTime && (
-              <div style={{ width: '100%', flexBasis: '100%' }}>
-                <Button
-                  color="teal"
-                  content="Next Steps – Who's the appointment for?"
-                  icon="right arrow"
-                  labelPosition="right"
-                  onClick={onClickNextSteps}
-                />
-              </div>
-            )}
-          </BottomContainer>
-        ) : (
-          <h4 style={{ textAlign: 'center' }}>{message}</h4>
-        )}
-      </StageContainer>
-    );
-  }
-}
+  return (
+    <StageContainer position={position}>
+      <ReactDayPicker
+        onDayClick={handleDayClick}
+        selectedDays={selectedDay}
+        modifiers={{ isHoliday, hasPassed }}
+        disabledDays={{
+          daysOfWeek: [0, 6],
+        }}
+      />
+      {selectedDay ? (
+        <BottomContainer>
+          {prettyDay}
+          <Form size="mini">
+            <Form.Group>
+              <Form.Field
+                control={Select}
+                options={timeOptions}
+                placeholder="What time?"
+                value={selectedTime}
+                onChange={onChangeSelectedTime}
+              />
+            </Form.Group>
+          </Form>
+          {selectedTime && (
+            <div style={{ width: '100%', flexBasis: '100%' }}>
+              <Button
+                color="teal"
+                content="Next Steps – Who's the appointment for?"
+                icon="right arrow"
+                labelPosition="right"
+                onClick={onClickNextSteps}
+              />
+            </div>
+          )}
+        </BottomContainer>
+      ) : (
+        <h4 style={{ textAlign: 'center' }}>{message}</h4>
+      )}
+    </StageContainer>
+  );
+};
 
 export default DayPicker;
